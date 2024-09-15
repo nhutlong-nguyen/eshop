d42e97bfd08c463cec5e66c84f7d96a9279e39bb
Long Nguyen <nhutlongnguyen.work@gmail.com>
2024-09-16 00:34:39 +0300

    added controllers, middleware, routes

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+const router = express.Router();
+import {
+  getProduct,
+  getProductById,
+} from "../controllers/productController.js";
+
+router.route("/").get(getProduct);
+router.route("/:id").get(getProductById);
+
+export default router;
